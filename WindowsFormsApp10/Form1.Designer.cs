3013d99575fab9faa7e8dbdfe4c2eeeaf7095a53
Bebrius <28-1@305-03>
2022-12-09 14:14:35 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp10/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿
+namespace WindowsFormsApp10
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3});
+            this.dataGridView1.Location = new System.Drawing.Point(26, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 48;
+            this.dataGridView1.Size = new System.Drawing.Size(369, 154);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Месяц 1";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Месяц 2";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Месяц 3";
+            this.Column3.Name = "Column3";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(425, 35);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(144, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(458, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "значение";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(425, 71);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(144, 74);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Изменить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(425, 167);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(144, 78);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "заполнить всё рандомными значениями";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(26, 187);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(125, 58);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Максимальное значение";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(157, 187);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(120, 58);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "Наибольшая сумма за квартал";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(283, 187);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(112, 58);
+            this.button5.TabIndex = 11;
+            this.button5.Text = "Максимальная общая зп";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(581, 257);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ WindowsFormsApp10/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp10
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            for (int i = 0; i < 11; i++)
+            {
+                dataGridView1.Rows.Add();
+
+            }
+            for (int i = 0; (i<=(dataGridView1.Rows.Count-1)); i++)
+            {
+                dataGridView1.Rows[i].HeaderCell.Value = (i+1).ToString();
+            }
+            
+        }
+        Random rand = new Random();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dataGridView1.CurrentCell.Value = textBox1.Text;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            for(int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < dataGridView1.Rows.Count; j++)
+                {
+                    dataGridView1.Rows[j].Cells[i].Value = rand.Next(16000,30000);
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int max = 0;
+            for (int i = 0; i < 3; i++)
+            {
+                max = int.Parse(dataGridView1[i, 0].Value.ToString());
+                for (int j = 0; j < dataGridView1.Rows.Count; j++)
+                {
+                    if (int.Parse(dataGridView1[i, j].Value.ToString()) >= max)
+                    {
+                        max = int.Parse(dataGridView1[i, j].Value.ToString());
+                    }  
+                }
+            }
+            MessageBox.Show($"Максимальная зарплата:{max}");
+        }
+        
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int[] arr = new int[12];
+            for (int i = 0; i < 12; i++)
+            {
+                int sum = 0;
+                foreach (DataGridViewCell cell in dataGridView1.Rows[i].Cells)
+                {
+                    int x = 0;
+                    sum += int.TryParse(cell.Value == null ? "0" : cell.Value.ToString(), out x) ? x : 0;
+                }
+                arr[i] = sum;
+            }
+
+            MessageBox.Show($"Наибольшую сумму за квартал получил работник №{(arr.ToList().IndexOf(arr.Max()))+1} его сумма зп за квартал: {arr.Max()}");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            int[] arr = new int[3];
+                for (int f = 0; f < 3; f++) {
+                int sum = (int)dataGridView1.Rows[0].Cells[f].Value;
+                for (int i = 0; i < dataGridView1.RowCount; i++)
+                {
+   
+                        sum += (int)dataGridView1.Rows[i].Cells[f].Value;
+
+                }
+                arr[f] = sum;
+            }
+            
+            MessageBox.Show($"Максимальная  общая зарплата сотрудников была в месяце №{(arr.ToList().IndexOf(arr.Max()))+1} сумма зп:{arr.Max()}");
+        }
+    }
+}
